354e015ffe826678a8d8dc9abd3a8ca4e6fc6d4e
misocho <misochobrian@gmail.com>
2019-09-24 15:46:17 +0300

    updates webpack configurations

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.join(__dirname, '/build'),
-        filename: 'index_bundle.js',
-        publicPath: '/'
+        path: path.join(__dirname, 'build'),
+        filename: 'index_bundle.js'
     },
     module: {
         rules: [
